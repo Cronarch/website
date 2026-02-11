beceff2f281f38a6fd981592bf60d127f7af742d
Caleb Brown <calebnbrown2004@gmail.com>
2026-02-11 13:54:04 -0600

    fix: remove unused icons

--- src/components/ui/home/hero.tsx
+++ src/components/ui/home/hero.tsx
@@ -1,7 +1,7 @@
 import { Link } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
 import { HeroHeader } from '../../header'
-import { AudioLines, ChevronRight, Github, ImageIcon, Lightbulb, Mic2, Paperclip, Plus, ShoppingBag, Telescope } from 'lucide-react'
+import { ChevronRight } from 'lucide-react'
 
 export default function HeroSection() {
     return (
