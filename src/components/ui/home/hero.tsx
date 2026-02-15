3c73f6767a6049a419885fa06142f224788ce332
Caleb Brown <calebnbrown2004@gmail.com>
2026-02-14 17:42:32 -0800

    lint: update files to fix linting

--- src/components/ui/home/hero.tsx
+++ src/components/ui/home/hero.tsx
@@ -1,56 +1,60 @@
-import { Link } from '@tanstack/react-router'
-import { Button } from '@/components/ui/button'
-import { HeroHeader } from '../../header'
-import { ChevronRight } from 'lucide-react'
+import { Link } from "@tanstack/react-router";
+import { ChevronRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { HeroHeader } from "./Header";
 
 export default function HeroSection() {
-    return (
-        <>
-            <HeroHeader />
-            <main className="overflow-hidden">
-                <section className="bg-background">
-                    <div className="relative py-32">
-                        <div className="mask-radial-from-45% mask-radial-to-75% mask-radial-at-top mask-radial-[75%_100%] mask-t-from-50% aspect-2/3 lg:aspect-9/4 absolute inset-0 md:aspect-square dark:opacity-5">
-                            <img
-                                src="https://images.unsplash.com/photo-1740516367177-ae20098c8786?q=80&w=2268&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                alt="hero background"
-                                width={2268}
-                                height={1740}
-                                className="h-full w-full object-cover object-top"
-                            />
-                        </div>
-                        <div className="relative z-10 mx-auto w-full max-w-5xl px-6">
-                            <div
-                                aria-hidden
-                                className="perspective-near mask-radial-at-left mask-radial-from-65% mask-radial-to-90% mask-radial-[75%_100%] min-w-2xl mb-6 ml-auto max-w-3xl scale-90 py-12 pl-6 sm:mb-12 md:pl-12 lg:mb-20">
-                                <div className="mask-radial-from-35% aspect-3/2 mask-radial-to-75% pointer-events-none relative mx-auto max-w-xl opacity-75 mix-blend-darken">
-                                    <div className="bg-background absolute inset-0 mix-blend-overlay"></div>
-                                    <img
-                                        src="https://images.unsplash.com/photo-1634595947394-87012e7b12ba?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                        alt="watch in dark"
-                                        className="not-dark:invert dark:mix-blend-lighten"
-                                        width={1340}
-                                        height={560}
-                                    />
-                                </div>
-                            </div>
-                            <div className="mx-auto max-w-md text-center">
-                                <h1 className="text-balance font-serif text-4xl font-medium sm:text-5xl">Save time. Make money.</h1>
-                                <p className="text-muted-foreground mt-4 text-balance">Cronarch is your all-in-one platform for automating your business.</p>
+	return (
+		<>
+			<HeroHeader />
+			<main className="overflow-hidden">
+				<section className="bg-background">
+					<div className="relative py-32">
+						<div className="mask-radial-from-45% mask-radial-to-75% mask-radial-at-top mask-radial-[75%_100%] mask-t-from-50% aspect-2/3 lg:aspect-9/4 absolute inset-0 md:aspect-square dark:opacity-5">
+							<img
+								src="https://images.unsplash.com/photo-1740516367177-ae20098c8786?q=80&w=2268&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+								alt="hero background"
+								width={2268}
+								height={1740}
+								className="h-full w-full object-cover object-top"
+							/>
+						</div>
+						<div className="relative z-10 mx-auto w-full max-w-5xl px-6">
+							<div
+								aria-hidden
+								className="perspective-near mask-radial-at-left mask-radial-from-65% mask-radial-to-90% mask-radial-[75%_100%] min-w-2xl mb-6 ml-auto max-w-3xl scale-90 py-12 pl-6 sm:mb-12 md:pl-12 lg:mb-20"
+							>
+								<div className="mask-radial-from-35% aspect-3/2 mask-radial-to-75% pointer-events-none relative mx-auto max-w-xl opacity-75 mix-blend-darken">
+									<div className="bg-background absolute inset-0 mix-blend-overlay"></div>
+									<img
+										src="https://images.unsplash.com/photo-1634595947394-87012e7b12ba?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+										alt="watch in dark"
+										className="not-dark:invert dark:mix-blend-lighten"
+										width={1340}
+										height={560}
+									/>
+								</div>
+							</div>
+							<div className="mx-auto max-w-md text-center">
+								<h1 className="text-balance font-serif text-4xl font-medium sm:text-5xl">
+									Save time. Make money.
+								</h1>
+								<p className="text-muted-foreground mt-4 text-balance">
+									Cronarch is your all-in-one platform for automating your
+									business.
+								</p>
 
-                                <Button
-                                    asChild
-                                    className="mt-6 pr-1.5">
-                                    <Link to="/" hash="link">
-                                        <span className="text-nowrap">Get Started</span>
-                                        <ChevronRight className="opacity-50" />
-                                    </Link>
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </>
-    )
+								<Button asChild className="mt-6 pr-1.5">
+									<Link to="/" hash="link">
+										<span className="text-nowrap">Get Started</span>
+										<ChevronRight className="opacity-50" />
+									</Link>
+								</Button>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+		</>
+	);
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,18 @@
-import HeroSection from '@/components/ui/home/hero'
-import Team from '@/components/ui/home/team'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import HeroSection from "@/components/ui/home/hero";
+import Team from "@/components/ui/home/team";
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-  return (
-    <div>
-      <HeroSection />
-      <section className="bg-background @container pt-32">
-        <div className="mx-auto">
-          <Team />
-        </div>
-      </section>
-    </div>
-  )
+	return (
+		<div>
+			<HeroSection />
+			<section className="bg-background @container pt-32">
+				<div className="mx-auto">
+					<Team />
+				</div>
+			</section>
+		</div>
+	);
 }
